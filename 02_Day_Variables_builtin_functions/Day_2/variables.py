140973ba66bda74afde9840e6adf811bf8071815
LebohangMashinini <palesal613@gmail.com>
2025-02-07 11:50:35 +0200

    done with day 2

---
+++ 02_Day_Variables_builtin_functions/Day_2/variables.py
@@ -0,0 +1,102 @@
+#Day 2: 30 Days of python programming
+#Exercise: Level 1
+
+#3
+first_name = "Lee"
+#4
+last_name = "Mashinini"
+#5
+full_name = "Lee Mashinini"
+#6
+country = "South Afica"
+#7
+city = "Johannesburg"
+#8
+age = 20
+#9
+year = 2025
+#10
+is_married = False
+#11
+is_true = True
+#12
+is_light_on = True
+#13
+fav_artist, fav_song_by_artist, fav_album_by_artsit = "Frank Ocean", "self control", "blond"
+
+#Exercises: Level 2
+
+#1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(fav_artist))
+print(type(fav_song_by_artist))
+print(type(fav_album_by_artsit))
+
+#2
+print(len(first_name))
+
+#3
+print(len(first_name) == len(last_name))
+
+#4
+num_one = 5
+num_two = 4
+
+#5
+total = num_one + num_two
+print(total)
+
+#6
+diff = num_two - num_one
+print(diff)
+
+#7
+product = num_two * num_one
+print(product)
+
+#8
+division = num_one / num_two
+print(division)
+
+#9
+remainder = num_two % num_one
+print(remainder)
+
+#10
+exp = num_one ** num_two
+print(exp)
+
+#11
+floor_divison = num_one // num_two
+print(floor_divison)
+
+#12
+#i
+pi = 3.142
+r = 30 
+area_of_circle = pi * (r*r)
+print(area_of_circle)
+
+#ii
+circum_of_circle = 2 * r * pi
+print(circum_of_circle)
+
+#iii
+radius = input("enter radius: ")
+area = pi * (radius * radius)
+print(area)
+
+#13
+firstName = input("Enter first name: ")
+lastName = input("Enter last name: ")
+country = input("Enter your country: ")
+age = input("Enter your age: ")
