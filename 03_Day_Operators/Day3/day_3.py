7fd80ebd0f40f684e104ffcffbff318d64855978
LebohangMashinini <palesal613@gmail.com>
2025-02-08 18:33:07 +0200

    almost done

--- 03_Day_Operators/Day3/day_3.py
+++ 03_Day_Operators/Day3/day_3.py
@@ -39,3 +39,51 @@ circumference = 2 * pi * radius
 print(f"The circumference of a circle is {circumference}")
 
 #8
+#x intercept
+#equation: 2x - 2
+y_to_zero = 0
+x_to_zero = 0
+x_intercept = 1
+y_intercept = -2
+
+slope = (y_intercept - y_to_zero) / (x_to_zero - x_intercept)
+print(slope)
+
+#9
+x1 = 2
+x2 = 2
+y1 = 6
+y2 = 10
+m = (y2 - y1) / (x2 - x1)
+print(m)
+
+#10
+print(slope == m)
+print(slope > m)
+print(slope < m)
+print(slope != m)
+print(slope >= m)
+print(slope <= m)
+
+#11
+y = 0
+x = -3
+value_y = -3 ** 2 + 6 * -3 + 9
+print(value_y)
+
+#12
+len_python = len("python")
+len_dragon = len("dragon")
+print(len_python != len_dragon)
+
+#13
+if "on" in "python" and "dragon":
+    print(True)
+
+#14
+sentence = "I hope this course is not full of jargon"
+for word in sentence:
+    if "jargon" in sentence:
+        print(True)
+
+#15
