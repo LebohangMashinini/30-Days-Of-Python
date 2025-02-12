e11a0070b54daf45e40add2c92d3ebb3db2cad31
kyle <kkndlovu9@gmail.com>
2025-02-12 10:59:33 +0200

    halfway there

---
+++ 04_Day_Strings/Day4/Day4.py
@@ -0,0 +1,72 @@
+#Exercises Day 4
+#1
+
+# first = "Thirty"
+# second = "Days"
+# third = "Of"
+# forth = "Python"
+
+# print(first + second + third + forth)
+
+# #2
+# a = "Coding"
+# b = "For"
+# c = "All"
+
+# print(a + b + c)
+
+# #3
+# company = "Coding For All"
+
+# #4
+# print(company)
+ 
+# #5
+# print(len(company))
+
+# #6
+# print(company.upper())
+
+# #7
+# print(company.lower())
+
+# #8
+# print(f"Capitalize: {company.capitalize()}")
+# print(f"Title: {company.title()}")
+# print(f"Swapcase: {company.swapcase()}")
+
+# #9
+# sentence = "Coding For All"
+# print(sentence[7:])
+
+# #10
+# sentence1 = "Coding For All"
+# ind = sentence1.index("Coding")
+# find = sentence1.find("Coding")
+
+# #11
+# sentence2 = "Coding For All"
+# x = sentence2.replace("Coding", "Python")
+# print(x)
+
+#12
+sentence3 = "Python for Everyone"
+y = sentence3.replace("for Everyone", "for All")
+print(y)
+
+#13
+string = "Coding For All"
+z = string.split(" ")
+print(z)
+
+#14
+socials = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+j = socials.split(",")
+print(j)
+
+#15
+print(string[0])
+
+#16
+print(string[-1])
+
