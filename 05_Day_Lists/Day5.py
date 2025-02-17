090a37b229387f9d29697af0c59e442aec0899a5
LebohangMashinini <palesal613@gmail.com>
2025-02-17 17:30:08 +0200

    did some of day 5

---
+++ 05_Day_Lists/Day5.py
@@ -0,0 +1,47 @@
+#Day 5
+#Exercises
+
+#1
+lis = []
+
+#2
+names = ["Lee", "Reba", "Neo", "Palesa", "Kg", "Lesedi", "Andy"]
+
+#3
+print(f"Length of the list of names: {len(names)}")
+
+#4
+first_name = names[0]
+print(f"first item: {first_name}")
+middle_name = names[3]
+print(f"middle item {middle_name}")
+last_name = names[6]
+print(f"last item: {last_name}")
+
+#5
+mixed_data_type = ["Lee", 20, 154, "single", "1020 Nyati Str"]
+
+#6
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+#7
+print(it_companies)
+
+#8
+print(f"Number of companies: {len(it_companies)}")
+
+#9
+first_company = it_companies[0]
+print(first_company)
+second_company = it_companies[3]
+print(second_company)
+last_company = it_companies[6]
+print(last_company)
+
+
+#10
+
+
+#11
+it_companies.insert(7, "Samsung")
+print(it_companies)
