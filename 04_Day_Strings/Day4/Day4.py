b38b8cec404cccb834d93954139ec2d15fde94af
LebohangMashinini <palesal613@gmail.com>
2025-02-21 09:42:16 +0200

    done with day 4

--- 04_Day_Strings/Day4/Day4.py
+++ 04_Day_Strings/Day4/Day4.py
@@ -50,38 +50,107 @@
 # print(x)
 
 #12
-sentence3 = "Python for Everyone"
-y = sentence3.replace("for Everyone", "for All")
+# sentence3 = "Python for Everyone"
+# y = sentence3.replace("for Everyone", "for All")
+# print(y)
+
+# #13
+# string = "Coding For All"
+# z = string.split(" ")
+# print(z)
+
+# #14
+# socials = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+# j = socials.split(",")
+# print(j)
+
+# #15
+# string1 = "_Coding For All_"
+# print(string1[0])
+
+# #16
+# print(string1[-1])
+
+# #17
+# print(string1[10])
+
+# #18
+# create_acronym = "Python For Everyone"
+# acronym = ''.join(word[0].upper() for word in create_acronym.split())
+# print(acronym)  
+
+# #19
+# create_acrnym = "Coding For All"
+# acrnym = ''.join(word[0].upper() for word in create_acrnym.split())
+# print(acrnym)
+
+# #20
+# index = create_acrnym.index("C")
+# print(index)
+
+# #21
+# ind = create_acrnym.index("F")
+# print(ind)
+
+# #22
+# x = create_acrnym.rfind("l")
+# print(x)
+
+#23
+sentence = "You cannot end a sentence with because because because is a conjunction"
+y = sentence.find("because")
 print(y)
 
-#13
-string = "Coding For All"
-z = string.split(" ")
+#24
+z = sentence.rindex("because")
 print(z)
 
-#14
-socials = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
-j = socials.split(",")
-print(j)
+#25
+j = z + len("because")
+print(sentence[y:z])
 
-#15
-string1 = "_Coding For All_"
-print(string1[0])
+#26
+#27
+#28
+substring = "Coding for all"
+print(substring.startswith("Coding"))
 
-#16
-print(string1[-1])
+#29
+print(substring.endswith("coding"))
 
-#17
-print(string1[10])
+#30
+given_str = "  Coding For All  "
+print(given_str)
 
-#18
-create_acronym = "Python For Everyone"
-acronym = ''.join(word[0].upper() for word in create_acronym.split())
-print(acronym)  
+#31
+print("30DaysOfPython".isidentifier())
+print("thirty_days_of_python".isidentifier())
 
-#19
-create_acrnym = "Coding For All"
-acrnym = ''.join(word[0].upper() for word in create_acrnym.split())
-print(acrnym)
+#32
+py_libraries = ["Django", "Flask", "Pyramid", "Falcon"]
+x = " # ".join(py_libraries)
+print(x)
+
+#33
+print("I am enjoying this challenge\nI just wonder what is next")
+
+#34
+print("Name\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki")
+
+#35
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area} meters square.")
+
+#36
+a = 8
+b = 6
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {round(a*b, 2)}")
+print(f"{a} / {b} = {a/b}")
+print(f"{a} % {b} = {a%b}")
+print(f"{a} // {b} = {a//b}")
+print(f"{a} ** {b} = {a**b}")
 
 
