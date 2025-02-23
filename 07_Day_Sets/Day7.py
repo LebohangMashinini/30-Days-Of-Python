ac5caba89a6e4cb1d029cc65ae61cd4f58ed67bb
LebohangMashinini <palesal613@gmail.com>
2025-02-23 16:13:41 +0200

    done with level 2 of day 7

--- 07_Day_Sets/Day7.py
+++ 07_Day_Sets/Day7.py
@@ -1,28 +1,59 @@
 #Exercises: Day 7
 #Exercises: Level 1
 # sets
-it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
 
-age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+# #1
+# print(len(it_companies))
+
+# #2
+# it_companies.add("Twitter")
+# print(it_companies)
+
+# #3
+# more_it_companies = {'Samsung', 'Dell', 'Intel','HP'}
+# it_companies.update(more_it_companies)
+# print(it_companies)
+
+# #4
+# it_companies.remove("Facebook")
+# print(it_companies)
+
+# #5
+# difference = "The remove method will raise an error if the specified item does not exist and Discard method does not raise an error."
+# print(f"What is the difference between remove and discard? {difference}")
+
+
+#Exercises: Level 2
+a = {19, 22, 24, 20, 25, 26}
+b = {19, 22, 20, 25, 26, 24, 28, 27}
 
 #1
-print(len(it_companies))
+print(a.union(b))
+
 
 #2
-it_companies.add("Twitter")
-print(it_companies)
+print(a.intersection(b))
 
 #3
-more_it_companies = {'Samsung', 'Dell', 'Intel','HP'}
-it_companies.update(more_it_companies)
-print(it_companies)
+print(a.issubset(b))
 
 #4
-it_companies.remove("Facebook")
-print(it_companies)
+print(a.isdisjoint(b))
 
 #5
-difference = "The remove method will raise an error if the specified item does not exist and Discard method does not raise an error."
-print(f"What is the difference between remove and discard? {difference}")
+a.update(b)
+b.update(a)
 
+#6
+a.symmetric_difference(b)
+print(a)
 
+#7
+del a
+del b
+
+#Exercises: Level 3
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+#1
